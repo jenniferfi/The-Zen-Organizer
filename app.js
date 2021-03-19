7363e46b7f7ce0897ca2755a202f983b2ceb7cb6
jenniferfi <jennifer.finnila@gmail.com>
2021-03-19 16:58:16 +0200

    refactored code, changed place of widgets

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
 $(document).ready(() =>{
     $('#calendar').load('widgets/date.html');
     $('#weather').load('widgets/weather.html');
-    $('#pomodoro').load('widgets/pomodoro.html');
     $('#box3').load('widgets/todo.html');
+    $('#box4').load('widgets/pomodoro.html');
 });
